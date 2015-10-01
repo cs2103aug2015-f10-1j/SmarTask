07494d2c3fe5ba371be744828c5de69dea36a352
Zaktansnake <david_chongym@outlook.com>
2015-10-02 02:26:18 +0800

    Added a Testing UI for SmarTask GUI
    Testing a possible UI design for SmarTask GUI.

--- src/GUI.java
+++ src/GUI.java
@@ -1,5 +1,32 @@
-import javafx.*;
+import java.io.IOException;
 
-public class GUI {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
 	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Welcome to SmarTask!");
+		Button btn = new Button();
+		btn.setText("Enter SmartTask");
+		btn.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				System.out.println("Welcome to SmartTask!");
+			}
+		});
+		
+		StackPane root = new StackPane();
+		root.getChildren().add(btn);
+		primaryStage.setScene(new Scene(root, 300, 250));
+		primaryStage.show();
+	}
 }
