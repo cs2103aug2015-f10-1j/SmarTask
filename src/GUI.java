75de812c6c06d76b47d2a3a5e13217d6f28d0379
Zaktansnake <david_chongym@outlook.com>
2015-10-02 03:23:16 +0800

    Added Basic UI for SmarTask
    Added a basic UI for SmarTask to assist in constructing a walking
    skeleton for SmarTask.

--- src/GUI.java
+++ src/GUI.java
@@ -1,32 +1,75 @@
-import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+/**
+ * GUI is a class that receives commands from the user and pass it down to 
+ * This program will serve as the main window of interaction between user and SmarTask.
+ * 
+ * @author David Chong
+ *
+ */
+
 public class GUI extends Application {
+	
+	private static final String MESSAGE_WELCOME = "Welcome to SmarTask!";
+	private static final String MESSAGE_GUIDE = "In case you need a refresher, type \" help \" to get a list of all the SmarTask commands.";
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
 	
 	public void start(Stage primaryStage) {
-		primaryStage.setTitle("Welcome to SmarTask!");
-		Button btn = new Button();
-		btn.setText("Enter SmartTask");
+		primaryStage.setTitle("SmarTask Main Window");
+		primaryStage.show();
+		
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(25, 25, 25, 25));
+		
+		Text messageWelcome = new Text(MESSAGE_WELCOME);
+		messageWelcome.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+		grid.add(messageWelcome, 0, 0, 2, 1);
+		
+		Text messageGuide = new Text(MESSAGE_GUIDE);
+		messageGuide.setFont(Font.font("Tahoma", FontWeight.NORMAL, 15));
+		grid.add(messageGuide, 0, 0, 2, 5);
+		
+		TextField userInputField = new TextField();
+		grid.add(userInputField, 0, 4);
+		
+		Button btn = new Button("Enter");
+		HBox hbBtn = new HBox(10);
+		hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+		hbBtn.getChildren().add(btn);
+		grid.add(hbBtn, 1, 4);
+		
+		final Text actionTarget = new Text();
+		grid.add(actionTarget, 0, 5);
+		
 		btn.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent event) {
-				System.out.println("Welcome to SmartTask!");
+			
+			public void handle(ActionEvent e) {
+				actionTarget.setFill(Color.FIREBRICK);
+				actionTarget.setText("Command has been entered");
 			}
 		});
 		
-		StackPane root = new StackPane();
-		root.getChildren().add(btn);
-		primaryStage.setScene(new Scene(root, 300, 250));
-		primaryStage.show();
+		Scene scene = new Scene(grid, 700, 600);
+		primaryStage.setScene(scene);
 	}
 }
