805b98fcedd47dab6c5a688135241a61026b9f2d
Zaktansnake <david_chongym@outlook.com>
2015-10-07 19:21:32 +0800

    Edited SmarTaskController to reflect changes in SmarTaskUI.fxml
    Added SmarTaskController to reflect changes in SmarTaskUI.fxml with the
    additional of a new TextArea.

--- src/SmarTaskController.java
+++ src/SmarTaskController.java
@@ -12,36 +12,35 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
 public class SmarTaskController implements Initializable {
-
+  
 	@FXML // fx:id = "commandButton"
-	private Button commandButton;	//Value injected by FXMLoader
-	private TextArea displayWindow; //Value injected by FXMLoader
-	private TextArea taskWindow; //Value injected by FXMLoader
-	private TextArea inputWindow; //Value injected by FXMLoader
+	private Button commandButton;   //Value injected by FXMLoader
+	private TextArea mainWindow;    //Value injected by FXML Loader
+	private TextArea displayWindow;	//Value injected by FXMLoader
+	private TextArea taskWindow;    //Value injected by FXMLoader
+	private TextArea inputWindow;   //Value injected by FXMLoader
 	
 	@Override
 	public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
 		assert commandButton != null : "fx:id=\"commandButton\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
-        assert displayWindow != null : "fx:id=\"displayWindow\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
+        assert mainWindow != null : "fx:id=\"mainWindow\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
+		assert displayWindow != null : "fx:id=\"displayWindow\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
 		assert taskWindow != null : "fx:id=\"taskWindow\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
 		assert inputWindow != null : "fx:id=\"inputWindow\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
         
 		Logic logic = new Logic();
 		
-		final Text actionTarget = new Text();
-
 	    commandButton.setOnAction(new EventHandler<ActionEvent>() {
-
 	            public void handle(ActionEvent e) {
 	                String userCommand = inputWindow.getText();
 	                try {
-	                    logic.executeCommand(userCommand);
+	                    String textToDisplay = logic.executeCommand(userCommand);
+	                    String originalText = displayWindow.getText();
+	                    displayWindow.appendText(originalText + textToDisplay);
 	                } catch (FileNotFoundException e1) {
 	                    // TODO Auto-generated catch block
 	                    e1.printStackTrace();
 	                }
-	                actionTarget.setFill(Color.FIREBRICK);
-	                actionTarget.setText("Command has been entered");
 	            }
 	        });	
 	}	
