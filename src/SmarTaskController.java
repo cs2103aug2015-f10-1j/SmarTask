5bd3594316012a1729f95290063f3cb6cbb10ae2
Zaktansnake <david_chongym@outlook.com>
2015-10-07 21:37:23 +0800

    Added a testing process for user input and removed enter button from UI
    Added a self-testing process for UI to process user input and removed
    the enter button from the UI.

--- src/SmarTaskController.java
+++ src/SmarTaskController.java
@@ -15,7 +15,6 @@ import javafx.scene.input.KeyEvent;
 public class SmarTaskController implements Initializable {
   
 	@FXML
-	private Button commandButton;   //Value injected by FXMLoader
 	private TextArea mainWindow;    //Value injected by FXML Loader
 	private TextArea displayWindow;	//Value injected by FXMLoader
 	private TextArea taskWindow;    //Value injected by FXMLoader
@@ -23,34 +22,19 @@ public class SmarTaskController implements Initializable {
 	
 	@Override
 	public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
-		assert commandButton != null : "fx:id=\"commandButton\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
         assert mainWindow != null : "fx:id=\"mainWindow\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
 		assert displayWindow != null : "fx:id=\"displayWindow\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
 		assert taskWindow != null : "fx:id=\"taskWindow\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
 		assert inputWindow != null : "fx:id=\"inputWindow\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
-        
-		Logic logic = new Logic();
-		
-	    commandButton.setOnAction(new EventHandler<ActionEvent>() {
-	            public void handle(ActionEvent e) {
-	                String userCommand = inputWindow.getText();
-	                try {
-	                    logic.executeCommand(userCommand);
-	                } catch (FileNotFoundException e1) {
-	                    // TODO Auto-generated catch block
-	                    e1.printStackTrace();
-	                }
-	            }
-	        });
 	}
 	
 	@FXML
 	public void onEnter(KeyEvent ke) {
 		if(ke.getCode() == KeyCode.ENTER) {
-			System.out.println("Success!");
-			/*
-			Logic logic = new Logic();
+			// Logic logic = new Logic();
 			String userCommand = inputWindow.getText();
+			System.out.println(userCommand);
+			/*
 	        try {
 	            logic.executeCommand(userCommand);
 	        } catch (FileNotFoundException e1) {
