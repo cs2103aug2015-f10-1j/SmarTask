e55af29fc0a0ad4cf090bd4dab248f17ce59c3fe
bobby-lin <bobby.linjh@gmail.com>
2015-10-08 17:58:53 +0800

    Change return messages of delete and view method

--- src/Logic.java
+++ src/Logic.java
@@ -13,46 +13,46 @@ public class Logic {
     private static Storage storage; 
 
     public Logic (){
-	commandParser = new CommandParser();
-	taskList = new ArrayList <String>();
-	recList = new ArrayList <String>();
-	storage = new Storage();
-	currentList = new ArrayList <Integer>();
+        commandParser = new CommandParser();
+        taskList = new ArrayList <String>();
+        recList = new ArrayList <String>();
+        storage = new Storage();
+        currentList = new ArrayList <Integer>();
     }
 
     public static String executeCommand (String userInput) throws FileNotFoundException{
-	String displayMessage = "";
-	storage.createFile();
-	taskList = storage.retrieveTexts();
+        String displayMessage = "";
+        storage.createFile();
+        taskList = storage.retrieveTexts();
 
-	Command command = commandParser.parse(userInput);
-	switch (command.getCommandType()){
-	case ADD : 
-	    displayMessage = addTask(command);
-	    break;
-	case ADDRECURRENCE : 
-	    displayMessage = addRec(command);
-	    break;
-	case DELETE :
-	    displayMessage = deleteTask(command);
-	    break;
-	case VIEW :
-	    displayMessage = viewTask (command);
-	    break;
-	case UPDATE :
-	    displayMessage = updateTask(command);
-	    break;
-	    // case SEARCH:
-		//	displayMessage = searchTask(command);
-		//	break;
-	    case EXIT :
-		break;
-	    default :
-		// return Command.Type.INVALID;
-	}
-	
-	taskList.clear();
-	return displayMessage;
+        Command command = commandParser.parse(userInput);
+        switch (command.getCommandType()){
+            case ADD : 
+                displayMessage = addTask(command);
+                break;
+            case ADDRECURRENCE : 
+                displayMessage = addRec(command);
+                break;
+            case DELETE :
+                displayMessage = deleteTask(command);
+                break;
+            case VIEW :
+                displayMessage = viewTask (command);
+                break;
+            case UPDATE :
+                displayMessage = updateTask(command);
+                break;
+                // case SEARCH:
+                //	displayMessage = searchTask(command);
+                //	break;
+            case EXIT :
+                break;
+            default :
+                // return Command.Type.INVALID;
+        }
+
+        taskList.clear();
+        return displayMessage;
 
     }
 
@@ -61,28 +61,28 @@ public class Logic {
     // ================================================================
 
     private static String addTask(Command com) throws FileNotFoundException{
-	String message;
-	String detailStored = com.getTaskTime() + "#" + com.getTaskTitle();
-	taskList.add(detailStored);
-	sortForAdd();
-	storage.saveToFile(taskList); 
-	message = "add " + com.getTaskTitle() + " successful!";
-	return message;
+        String message;
+        String detailStored = com.getTaskTime() + "#" + com.getTaskTitle();
+        taskList.add(detailStored);
+        sortForAdd();
+        storage.saveToFile(taskList); 
+        message = "add " + com.getTaskTitle() + " successful!";
+        return message;
 
     }
 
     public static void sortForAdd(){
-	Collections.sort(taskList, new Comparator<String>() {
-	    DateFormat f = new SimpleDateFormat("dd/MM/yyyy hh:mm");
-	    @Override
-	    public int compare(String o1, String o2) {
-		try {
-		    return f.parse(o1).compareTo(f.parse(o2));
-		} catch (ParseException e) {
-		    throw new IllegalArgumentException(e);
-		}
-	    }
-	});
+        Collections.sort(taskList, new Comparator<String>() {
+            DateFormat f = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+            @Override
+            public int compare(String o1, String o2) {
+                try {
+                    return f.parse(o1).compareTo(f.parse(o2));
+                } catch (ParseException e) {
+                    throw new IllegalArgumentException(e);
+                }
+            }
+        });
     }
 
     // ================================================================
@@ -90,13 +90,13 @@ public class Logic {
     // ================================================================
 
     private static String addRec(Command com) throws FileNotFoundException{
-	String message;
-	storage = new Storage();     
-	String detailStored =  com.getRecurringPeriod() +"#" + com.getTaskTitle();
-	recList.add(detailStored);
-	storage.saveToFileRC(recList);
-	message =  "addrc " + com.getTaskTitle() + " successful!";
-	return message;
+        String message;
+        storage = new Storage();     
+        String detailStored =  com.getRecurringPeriod() +"#" + com.getTaskTitle();
+        recList.add(detailStored);
+        storage.saveToFileRC(recList);
+        message =  "addrc " + com.getTaskTitle() + " successful!";
+        return message;
 
     }
     /* 
@@ -115,9 +115,9 @@ public class Logic {
     }
      */
     public static void printArrayList (){
-	for (int i=0; i<taskList.size(); i++){
-	    System.out.println(taskList.get(i));
-	}
+        for (int i=0; i<taskList.size(); i++){
+            System.out.println(taskList.get(i));
+        }
     }
 
 
@@ -126,17 +126,17 @@ public class Logic {
     // ================================================================
 
     public static String deleteTask(Command com){
-	String message = "";
-	try{
-	    int index = currentList.get(com.getTaskNumber()-1);
-	    taskList.remove(index);
-	    currentList.remove(com.getTaskNumber()-1);
-	    storage.saveToFile(taskList); 
-	    message = "delete "+com.getTaskNumber() + " successful!";
-	}catch(Exception e){
-	    message = "Error. Invalid task number";
-	}
-	return message;
+        String message = "";
+        try{
+            int index = currentList.get(com.getTaskNumber()-1);
+            taskList.remove(index);
+            currentList.remove(com.getTaskNumber()-1);
+            storage.saveToFile(taskList); 
+            message = "delete task no. " + com.getTaskNumber() + " successfully!";
+        }catch(Exception e){
+            message = "Error. Invalid task number";
+        }
+        return message;
     }
 
     // ================================================================
@@ -144,39 +144,39 @@ public class Logic {
     // ================================================================
 
     public static String viewTask(Command com){
-	String message = "";
-	int index = 1;
+        String message = "";
+        int index = 1;
 
-	//	taskList = Storage.retrieveTexts();
-	for (int i=0; i<taskList.size(); i++){
-	    if (taskList.get(i).contains(com.getTaskTime())){
-		currentList.add(i);
-		String[] str = taskList.get(i).trim().split("#");
-		message += (index++) + ". " + str[1] + "\n";
-	    }	
-	}
+        //	taskList = Storage.retrieveTexts();
+        for (int i = 0; i<taskList.size(); i++){
+            if (taskList.get(i).contains(com.getTaskTime())){
+                currentList.add(i);
+                String[] str = taskList.get(i).trim().split("#");
+                message += (index++) + ". " + str[1] + " " + str[0] + "\n" ;
+            }	
+        }
 
-	return message;
+        return message;
     }
-    
- // ================================================================
+
+    // ================================================================
     // "Update" command methods
     // ================================================================
-    
+
     public static String updateTask(Command com) throws FileNotFoundException{
-    	String message = "";
-    	int taskListIndex = currentList.get(com.getTaskNumber()-1);
-    	String[] str = taskList.get(taskListIndex).trim().split("#");
-    	str[1] = com.getTaskTitle();
-    	String updateString = "";
-    	for (int i=0; i < str.length-1; i++){
-    	    updateString += (str[i] + "#");
-    	}
-    	updateString += str[str.length-1];
-    	taskList.set(taskListIndex, updateString);
-    	
-    	storage.saveToFile(taskList);
-    	return message;
+        String message = "";
+        int taskListIndex = currentList.get(com.getTaskNumber()-1);
+        String[] str = taskList.get(taskListIndex).trim().split("#");
+        str[1] = com.getTaskTitle();
+        String updateString = "";
+        for (int i=0; i < str.length-1; i++){
+            updateString += (str[i] + "#");
+        }
+        updateString += str[str.length-1];
+        taskList.set(taskListIndex, updateString);
+
+        storage.saveToFile(taskList);
+        return message;
     }
 
 
@@ -184,24 +184,24 @@ public class Logic {
     // "show to user" command methods
     // ================================================================
     private static String showToUser(String message){
-	//   System.out.println( message);
-	return message;
+        //   System.out.println( message);
+        return message;
     }
 }
 
 class LogicTest {
     public static void main(String[] arg) throws FileNotFoundException {
-	Logic logic = new Logic();
-	//logic.executeCommand("add <meeting with team-mates> <09/10/2015 18:00>");
-	//logic.executeCommand("add <testing program component> <09/10/2015 14:00>");
-	//logic.executeCommand("add <meeting with team-mates for integration> <08/10/2015 18:00>");
-	//logic.printArrayList();
-	System.out.print(logic.executeCommand("view <09/10/2015>"));
-	//System.out.print(logic.executeCommand("update <2> <UPDATED> <09/10/2015>"));
-	//System.out.print(logic.executeCommand("view <09/10/2015>"));
-	System.out.print(logic.executeCommand("delete <2> <09/10/2015>"));
-	System.out.print(logic.executeCommand("view <09/10/2015>"));
-	System.out.println("End of Test");
+        Logic logic = new Logic();
+        //logic.executeCommand("add <meeting with team-mates> <09/10/2015 18:00>");
+        //logic.executeCommand("add <testing program component> <09/10/2015 14:00>");
+        //logic.executeCommand("add <meeting with team-mates for integration> <08/10/2015 18:00>");
+        //logic.printArrayList();
+        System.out.print(logic.executeCommand("view <09/10/2015>"));
+        //System.out.print(logic.executeCommand("update <2> <UPDATED> <09/10/2015>"));
+        //System.out.print(logic.executeCommand("view <09/10/2015>"));
+        System.out.print(logic.executeCommand("delete <2> <09/10/2015>"));
+        System.out.print(logic.executeCommand("view <09/10/2015>"));
+        System.out.println("End of Test");
     }
 
 }
