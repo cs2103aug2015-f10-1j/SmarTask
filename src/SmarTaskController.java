fcdb5227e69564de9621d23d0a17397e54870dc4
Zaktansnake <david_chongym@outlook.com>
2015-10-15 18:16:02 +0800

    Altered SmarTaskController to add in new text area and remove hot fix
    Altered the onEnter method of SmarTaskController to remove the temporary
    hot fix for V0.1 testing and added in a more stable method for
    taskWindow to display tasks for the day. Added a injection for the tasks
    without a due date and a assert to check if the text area exists.

--- src/SmarTaskController.java
+++ src/SmarTaskController.java
@@ -14,30 +14,32 @@ public class SmarTaskController implements Initializable {
 	@FXML private TextArea mainWindow;    //Value injected by FXML Loader
 	@FXML private TextArea displayWindow;	//Value injected by FXMLoader
 	@FXML private TextArea taskWindow;    //Value injected by FXMLoader
+	@FXML private TextArea specialTaskWindow; //Value injected by FXMLoader
 	@FXML private TextField inputWindow;   //Value injected by FXMLoader
 	public Logic logic = new Logic();
+	public String viewCommand = "view today's task";
 	
 	@Override
 	public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
         assert mainWindow != null : "fx:id=\"mainWindow\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
 		assert displayWindow != null : "fx:id=\"displayWindow\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
 		assert taskWindow != null : "fx:id=\"taskWindow\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
+		assert specialTaskWindow != null : "fx:id=\"specialTaskWindow\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
 		assert inputWindow != null : "fx:id=\"inputWindow\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
+		
+		try {
+			String taskInitialDisplay = logic.executeCommand(viewCommand);
+			taskWindow.setText(taskInitialDisplay);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 	@FXML
 	public void onEnter(KeyEvent ke) {
 		if(ke.getCode() == KeyCode.ENTER) {
 			String userCommand = inputWindow.getText();
-			String viewCommand = "view <09/10/2015>";
-			
-			if(userCommand.contains("view")) {
-				viewCommand = userCommand;
-			} else if(userCommand.contains("08/10/2015")) {
-				viewCommand = "view <08/10/2015>";
-			}
-			
-			System.out.println(userCommand);
 			inputWindow.clear();
 	        try {
 	            String logicReturn = logic.executeCommand(userCommand);
