b24eb3fce54fe349ee647c54f24981431ba1f6cc
Zhang Yichi <kevincham1993@gmail.com>
2015-10-15 10:02:38 +0800

    new class CreateXMLFile

---
+++ src/main/CreateXMLFile.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package main;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
+/**
+ * @author user
+ *
+ */
+public class CreateXMLFile {
+
+	/**
+	 * 
+	 */
+	public CreateXMLFile() {
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
