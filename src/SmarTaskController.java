431d08f9185b08d4be066198e2e49febf47e1de6
Zaktansnake <david_chongym@outlook.com>
2015-10-19 01:47:12 +0800

    Altered SmarTaskController.java to accommodate for new Event text area.
    Edited the SmarTaskController to accommodate for the event text area
    added to SmarTaskUI.fxml. Also included additional key events for Ctrl+Z
    and Ctrl+Y for future undo and redo commands for SmarTask.

--- src/SmarTaskController.java
+++ src/SmarTaskController.java
@@ -21,10 +21,12 @@ public class SmarTaskController implements Initializable {
 
     @FXML private TextArea mainWindow;    //Value injected by FXML Loader
     @FXML private TextArea displayWindow;	//Value injected by FXMLoader
+    @FXML private TextArea eventWindow; //Value injected by FXMLoader
     @FXML private TextArea taskWindow;    //Value injected by FXMLoader
     @FXML private TextArea specialTaskWindow; //Value injected by FXMLoader
     @FXML private TextField inputWindow;   //Value injected by FXMLoader
     public String logDisplay;
+    public String eventDisplay;
     public String taskDeadlineDisplay;
     public String specialTaskDisplay;
 
@@ -32,15 +34,18 @@ public class SmarTaskController implements Initializable {
     public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
         assert mainWindow != null : "fx:id=\"mainWindow\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
         assert displayWindow != null : "fx:id=\"displayWindow\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
+        assert eventWindow != null : "fx:id=\"eventWindow\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
         assert taskWindow != null : "fx:id=\"taskWindow\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
         assert specialTaskWindow != null : "fx:id=\"specialTaskWindow\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
         assert inputWindow != null : "fx:id=\"inputWindow\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
         logDisplay = Logic.getMessageLog();
+        eventDisplay = Logic.getEvents();
         taskDeadlineDisplay = Logic.getDeadline();
         specialTaskDisplay = Logic.getFloatingTask();
 
         try {
             displayText(displayWindow, logDisplay);
+            displayText(eventWindow, eventDisplay);
             displayText(taskWindow, taskDeadlineDisplay);
             displayText(specialTaskWindow, specialTaskDisplay);
         } catch (Exception e) {
@@ -55,28 +60,50 @@ public class SmarTaskController implements Initializable {
     }
 
     @FXML
-    public void onEnter(KeyEvent ke) {
+    public void keyboardLog(KeyEvent ke) {
         if(ke.getCode() == KeyCode.ENTER) {
-            String userCommand = inputWindow.getText();
-            inputWindow.clear();
-            try {
-                Logic.executeCommand(userCommand);
-                displayWindow.clear();              
-                logDisplay = Logic.getMessageLog();
-                displayText(displayWindow, logDisplay);
-                
-                taskWindow.clear();
-                taskDeadlineDisplay = Logic.getDeadline();
-                displayText(taskWindow, taskDeadlineDisplay);
-                
-                specialTaskDisplay = Logic.getFloatingTask();
-                displayText(specialTaskWindow, specialTaskDisplay);
-            } catch (FileNotFoundException e1) {
-                e1.printStackTrace();
-            } catch (Exception e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+            enterKeyEvent();
+        } else if(ke.getCode() == KeyCode.UP) {
+        	upKeyEvent();
+        } else if(ke.getCode() == KeyCode.CONTROL) {
+        	if(ke.getCode() == KeyCode.Z) {
+        		controlZKeyEvent();
+        	} else if(ke.getCode() == KeyCode.Y) {
+        		controlYKeyEvent();
+        	}
         }
+    }
+    
+    public void enterKeyEvent() {
+    	String userCommand = inputWindow.getText();
+        inputWindow.clear();
+        try {
+            Logic.executeCommand(userCommand);
+            displayWindow.clear();              
+            logDisplay = Logic.getMessageLog();
+            displayText(displayWindow, logDisplay);
+            
+            taskWindow.clear();
+            taskDeadlineDisplay = Logic.getDeadline();
+            displayText(taskWindow, taskDeadlineDisplay);
+            
+            specialTaskDisplay = Logic.getFloatingTask();
+            displayText(specialTaskWindow, specialTaskDisplay);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void upKeyEvent() {
+    	String pastCommand = "Displays Last User Command";
+    	inputWindow.setText(pastCommand);
+    }
+    
+    public void controlZKeyEvent() {
+    	
+    }
+    
+    public void controlYKeyEvent() {
+    	
     }
 }
