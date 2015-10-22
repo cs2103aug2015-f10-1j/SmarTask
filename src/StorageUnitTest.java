b806a1544f10535b59d85aa07bc4593d5dff877e
Zhang Yichi <kevincham1993@gmail.com>
2015-10-23 02:38:08 +0800

    Add test for storage

---
+++ src/StorageUnitTest.java
@@ -0,0 +1,46 @@
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.PrintWriter;
+
+import org.junit.After;
+import org.junit.Test;
+
+import com.google.gson.Gson;
+
+
+
+public class StorageUnitTest {
+	private static final String DEFAULT_FILENAME = "storage.txt";
+	
+	File taskFile = new File(DEFAULT_FILENAME);
+	PrintWriter writer;
+	BufferedReader reader;
+	
+	/*
+	public StorageUnitTest() {
+		// TODO Auto-generated constructor stub
+	}
+	*/
+	
+	@Test
+	public void initTest() {
+		taskFile.delete();
+		
+		Storage storageTest = Storage.getInstance();
+		
+		assertEquals(true, taskFile.exists());		
+	}
+	
+	@Test
+	public void readFile() {
+		// do not know how to test...
+	}
+	
+	@After
+	public void clearTask() {
+		taskFile.delete();
+	}
+
+}
