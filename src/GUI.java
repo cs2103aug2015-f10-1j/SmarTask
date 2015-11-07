f300c7bb754af0a81ea11db4e06622b675d1e209
Zaktansnake <david_chongym@outlook.com>
2015-11-07 23:23:55 +0800

    Refactored UI Component
    Updated UI component to comply with java coding standards.

--- src/GUI.java
+++ src/GUI.java
@@ -29,137 +29,137 @@ import javafx.stage.Stage;
 
 public class GUI extends Application {
 
-	private static final String BUTTON_CHOOSELOCATION = "Choose Storage Location";
-	private static final String BUTTON_DEFAULTLOCATION = "Use Default Location";
-	private static final String COMMAND_FILEPATH = "setfilepath ";
-	private static final String FILE_SMARTASKUI = "SmarTaskUI.fxml";
-	private static final String MESSAGE_FXMLERROR = "Error! SmarTaskUI.fxml cannot be found!";
-	private static final String MESSAGE_FILEPATHERROR = "Error! Filepath.txt cannot be read!";
-	private static final String SCENE_TITLE = "SmarTask";
+    private static final String BUTTON_CHOOSELOCATION = "Choose Storage Location";
+    private static final String BUTTON_DEFAULTLOCATION = "Use Default Location";
+    private static final String COMMAND_FILEPATH = "setfilepath ";
+    private static final String FILE_SMARTASKUI = "SmarTaskUI.fxml";
+    private static final String MESSAGE_FXMLERROR = "Error! SmarTaskUI.fxml cannot be found!";
+    private static final String MESSAGE_FILEPATHERROR = "Error! Filepath.txt cannot be read!";
+    private static final String SCENE_TITLE = "SmarTask";
 
-	private Stage stage;
-	private String defaultFileLocation = System.getProperty("user.home") + "/Desktop";
-	private static String filePath = "../main/" + "filepath.txt";
-
-	public static void main(String[] args) {
-		launch(args);
-	}
-
-	public void start(Stage primaryStage) {
-		if (checkFile(filePath)) {
-			try {
-				Parent root = FXMLLoader.load(getClass().getResource(FILE_SMARTASKUI));
-				Scene scene = new Scene(root, 700, 800);
-				primaryStage.setTitle(SCENE_TITLE);
-				primaryStage.setScene(scene);
-				primaryStage.show();
-			} catch (IOException e) {
-				System.err.println(MESSAGE_FXMLERROR);
-			}
-		} else {
-			stage = primaryStage;
-			Scene scene = chooseFileScene(primaryStage);
-			primaryStage.setTitle(SCENE_TITLE);
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		}
-	}
-
-	private static boolean checkFile(String fileName) {
-		File file = new File(fileName);
-		FileReader fr = null;
-		BufferedReader br = null;
-		
-		try {
-			fr = new FileReader(file);
-			br = new BufferedReader(fr);
-			System.out.println(file.getAbsolutePath());
-			
-			if (br.readLine() != null) {
-				br.close();
-				return true;
-			} else {
-				br.close();
-				return false;
-			}
-		} catch (IOException e) {
-			System.err.println(MESSAGE_FILEPATHERROR);
-		}
-		
-		return false;
-	}
-
-	private Scene chooseFileScene(Stage primaryStage) {
-		final FileChooser fileChooser = new FileChooser();
-		final Button createFileButton = new Button(BUTTON_CHOOSELOCATION);
-		final Button defaultFileButton = new Button(BUTTON_DEFAULTLOCATION);
-
-		createFileButton.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(final ActionEvent e) {
-				File file = fileChooser.showSaveDialog(primaryStage);
-				
-				if (file != null) {
-					createFile(file);
-					
-					try {
-						stage.setScene(createSmarTaskScene());
-					} catch (IOException e1) {
-						e1.printStackTrace();
-					}
-				}
-			}
-		});
-
-		defaultFileButton.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(final ActionEvent e) {
-				try {
-					File file = new File(defaultFileLocation);
-					defaultFile(file);
-					stage.setScene(createSmarTaskScene());
-				} catch (IOException e2) {
-					e2.printStackTrace();
-				}
-			}
-		});
-
-		final GridPane inputGridPane = new GridPane();
-		GridPane.setConstraints(createFileButton, 0, 0);
-		GridPane.setConstraints(defaultFileButton, 1, 0);
-		inputGridPane.setHgap(6);
-		inputGridPane.setVgap(6);
-		inputGridPane.getChildren().addAll(createFileButton, defaultFileButton);
-
-		final Pane rootGroup = new VBox(12);
-		rootGroup.getChildren().addAll(inputGridPane);
-		rootGroup.setPadding(new Insets(12, 12, 12, 12));
-
-		Scene scene = new Scene(rootGroup);
-		return scene;
-	}
-
-	protected Scene createSmarTaskScene() throws IOException {
-		Parent root = FXMLLoader.load(getClass().getResource(FILE_SMARTASKUI));
-		Scene scene = new Scene(root, 700, 800);
-		return scene;
-	}
-
-	private void createFile(File file) {
-		try {
-			String command = COMMAND_FILEPATH + file.getAbsolutePath();
-			Logic.executeCommand(command);
-		} catch (Exception e) {
-			Logger.getLogger(GUI.class.getName()).log(Level.SEVERE, null, e);
-		}
-	}
-
-	private void defaultFile(File file) {
-		try {
-			String command = COMMAND_FILEPATH + file.getAbsolutePath();
-			Logic.executeCommand(command);
-		} catch (Exception e) {
-			Logger.getLogger(GUI.class.getName()).log(Level.SEVERE, null, e);
-		}
-	}
+    private Stage stage;
+    private String defaultFileLocation = System.getProperty("user.home") + "/Desktop";
+    private static String filePath = "../main/" + "filepath.txt";
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    public void start(Stage primaryStage) {
+        if (checkFile(filePath)) {
+            try {
+                Parent root = FXMLLoader.load(getClass().getResource(FILE_SMARTASKUI));
+                Scene scene = new Scene(root, 700, 800);
+                primaryStage.setTitle(SCENE_TITLE);
+                primaryStage.setScene(scene);
+                primaryStage.show();
+            } catch (IOException e) {
+                System.err.println(MESSAGE_FXMLERROR);
+            }
+        } else {
+    	    stage = primaryStage;
+    	    Scene scene = chooseFileScene(primaryStage);
+    	    primaryStage.setTitle(SCENE_TITLE);
+    	    primaryStage.setScene(scene);
+    	    primaryStage.show();
+        }
+    }
+    
+    private static boolean checkFile(String fileName) {
+        File file = new File(fileName);
+        FileReader fr = null;
+        BufferedReader br = null;
+        
+        try {
+            fr = new FileReader(file);
+            br = new BufferedReader(fr);
+            System.out.println(file.getAbsolutePath());
+            
+            if (br.readLine() != null) {
+                br.close();
+                return true;
+            } else {
+                br.close();
+                return false;
+            }
+        } catch (IOException e) {
+            System.err.println(MESSAGE_FILEPATHERROR);
+        }
+        
+        return false;
+    }
+    
+    private Scene chooseFileScene(Stage primaryStage) {
+        final FileChooser fileChooser = new FileChooser();
+        final Button createFileButton = new Button(BUTTON_CHOOSELOCATION);
+        final Button defaultFileButton = new Button(BUTTON_DEFAULTLOCATION);
+        
+        createFileButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(final ActionEvent e) {
+                File file = fileChooser.showSaveDialog(primaryStage);
+                
+                if (file != null) {
+                    createFile(file);
+                    
+                    try {
+                        stage.setScene(createSmarTaskScene());
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        });
+        
+        defaultFileButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(final ActionEvent e) {
+                try {
+                    File file = new File(defaultFileLocation);
+                    defaultFile(file);
+                    stage.setScene(createSmarTaskScene());
+                } catch (IOException e2) {
+                    e2.printStackTrace();
+                }
+            }
+        });
+        
+        final GridPane inputGridPane = new GridPane();
+        GridPane.setConstraints(createFileButton, 0, 0);
+        GridPane.setConstraints(defaultFileButton, 1, 0);
+        inputGridPane.setHgap(6);
+        inputGridPane.setVgap(6);
+        inputGridPane.getChildren().addAll(createFileButton, defaultFileButton);
+        
+        final Pane rootGroup = new VBox(12);
+        rootGroup.getChildren().addAll(inputGridPane);
+        rootGroup.setPadding(new Insets(12, 12, 12, 12));
+        
+        Scene scene = new Scene(rootGroup);
+        return scene;
+    }
+    
+    protected Scene createSmarTaskScene() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource(FILE_SMARTASKUI));
+        Scene scene = new Scene(root, 700, 800);
+        return scene;
+    }
+    
+    private void createFile(File file) {
+        try {
+            String command = COMMAND_FILEPATH + file.getAbsolutePath();
+            Logic.executeCommand(command);
+        } catch (Exception e) {
+            Logger.getLogger(GUI.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+    
+    private void defaultFile(File file) {
+        try {
+            String command = COMMAND_FILEPATH + file.getAbsolutePath();
+            Logic.executeCommand(command);    
+        } catch (Exception e) {
+            Logger.getLogger(GUI.class.getName()).log(Level.SEVERE, null, e);
+        }
+   }
 }
--- src/SmarTaskController.java
+++ src/SmarTaskController.java
@@ -25,232 +25,232 @@ import javafx.stage.Stage;
 
 public class SmarTaskController implements Initializable {
 
-	@FXML
-	private TextArea displayWindow;
-	@FXML
-	private TextArea taskWindow;
-	@FXML
-	private TextField inputWindow;
-	
-	private static final String COMMAND_HELP = "help";
-	private static final String COMMAND_UNDO = "undo";
-	private static final String COMMAND_REDO = "redo";
-	private static final String COMMAND_EXIT = "exit";
-	private static final String MESSAGE_WELCOME = "Welcome to SmarTask! In case you need a refresher, please type \"help\" to get a list of all the SmarTask commands.";
-	private static final String MESSAGE_INVALIDCOMMAND = "Error! Invalid Command sent to Logic!";
-	private static final String MESSAGE_INVALIDUNDO = "Error! Invalid Undo Command sent to Logic!";
-	private static final String MESSAGE_INVALIDREDO = "Error! Invalid Redo Command sent to Logic!";
-	private static final String TASK_DEADLINE = "Deadline Tasks:";
-	private static final String TASK_EVENTS = "Event Tasks:";
-	private static final String TASK_FLOATING = "Floating Tasks:";
-	private static final String TASK_RECURRING = "Recurring Tasks:";
-	private final KeyCombination crtlZ = new KeyCodeCombination(KeyCode.Z, KeyCombination.CONTROL_DOWN);
-	private final KeyCombination crtlY = new KeyCodeCombination(KeyCode.Y, KeyCombination.CONTROL_DOWN);
-	
-	private static String logDisplay;
-	private static String deadlineTasks;
-	private static String eventTasks;
-	private static String floatingTasks;
-	private static String recurringTasks;
-	private static Stack<String> pastCommands;
-	private static Stack<String> poppedCommands;
+    @FXML
+    private TextArea displayWindow;
+    @FXML
+    private TextArea taskWindow;
+    @FXML
+    private TextField inputWindow;
+    
+    private static final String COMMAND_HELP = "help";
+    private static final String COMMAND_UNDO = "undo";
+    private static final String COMMAND_REDO = "redo";
+    private static final String COMMAND_EXIT = "exit";
+    private static final String MESSAGE_WELCOME = "Welcome to SmarTask! In case you need a refresher, please type \"help\" to get a list of all the SmarTask commands.";
+    private static final String MESSAGE_INVALIDCOMMAND = "Error! Invalid Command sent to Logic!";
+    private static final String MESSAGE_INVALIDUNDO = "Error! Invalid Undo Command sent to Logic!";
+    private static final String MESSAGE_INVALIDREDO = "Error! Invalid Redo Command sent to Logic!";
+    private static final String TASK_DEADLINE = "Deadline Tasks:";
+    private static final String TASK_EVENTS = "Event Tasks:";
+    private static final String TASK_FLOATING = "Floating Tasks:";
+    private static final String TASK_RECURRING = "Recurring Tasks:";
+    private final KeyCombination crtlZ = new KeyCodeCombination(KeyCode.Z, KeyCombination.CONTROL_DOWN);
+    private final KeyCombination crtlY = new KeyCodeCombination(KeyCode.Y, KeyCombination.CONTROL_DOWN);
+    
+    private static String logDisplay;
+    private static String deadlineTasks;
+    private static String eventTasks;
+    private static String floatingTasks;
+    private static String recurringTasks;
+    private static Stack<String> pastCommands;
+    private static Stack<String> poppedCommands;
 
-	/**
-	 * Checks if the variables referenced from the fxml file have been properly
-	 * referenced If not, method will return a error message
-	 * 
-	 * @param fxmlFileLocation
-	 *            the file location of the fxml file in the system
-	 * @param resources
-	 *            the resources associated with the fxml file
-	 */
-	@Override
-	public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
-		assert displayWindow != null : "fx:id=\"displayWindow\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
-		assert taskWindow != null : "fx:id=\"taskWindow\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
-		assert inputWindow != null : "fx:id=\"inputWindow\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
-		pastCommands = new Stack<String>();
-		poppedCommands = new Stack<String>();
+    /**
+     * Checks if the variables referenced from the fxml file have been properly
+     * referenced If not, method will return a error message
+     * 
+     * @param fxmlFileLocation
+     *            the file location of the fxml file in the system
+     * @param resources
+     *            the resources associated with the fxml file
+     */
+    @Override
+    public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
+        assert displayWindow != null : "fx:id=\"displayWindow\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
+        assert taskWindow != null : "fx:id=\"taskWindow\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
+        assert inputWindow != null : "fx:id=\"inputWindow\" was not injected: check your FXML file 'SmarTaskUI.fxml'.";
+        pastCommands = new Stack<String>();
+        poppedCommands = new Stack<String>();
+        
+        try {
+            updateDisplay();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        
+        displayWindow.setText(MESSAGE_WELCOME);
+        
+        displayWindow.textProperty().addListener(new ChangeListener<Object>() {
+            @Override
+            public void changed(ObservableValue<?> observable, Object oldValue, Object newValue) {
+                displayWindow.selectPositionCaret(displayWindow.getLength());
+                displayWindow.deselect();
+            }
+        });    
+    }
 
-		try {
-			updateDisplay();
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		
-		displayWindow.setText(MESSAGE_WELCOME);
-
-		displayWindow.textProperty().addListener(new ChangeListener<Object>() {
-			@Override
-			public void changed(ObservableValue<?> observable, Object oldValue, Object newValue) {
-				displayWindow.selectPositionCaret(displayWindow.getLength());
-				displayWindow.deselect();
-			}
-		});
+    /**
+     * Method updates the information display from Logic to the user so they can
+     * preview the information stored in SmarTask.
+     * 
+     * @throws ParseException
+     */
+    private void updateDisplay() throws ParseException {
+        logDisplay = Logic.getMessageLog();
+        deadlineTasks = Logic.getDeadline();
+        eventTasks = Logic.getEvents();
+        floatingTasks = Logic.getFloatingTask();
+        recurringTasks = Logic.getRecurringTask();
+        updateWindows(displayWindow, logDisplay);
+        updateWindows(taskWindow);
+    }
+    
+    private void updateWindows(TextArea display, String toDisplay) {
+        display.clear();
+        display.setText(toDisplay);
+        String emptyString = "";
+        display.appendText(emptyString);
+    }
+    
+    private void updateWindows(TextArea display) {
+        display.clear();
+	    String lineBreak = "\n";
+	    display.setText(TASK_DEADLINE);
+	    display.appendText(lineBreak);
+	    display.appendText(deadlineTasks);
+	    display.appendText(lineBreak);
+	    display.appendText(lineBreak);
+	    display.appendText(TASK_EVENTS);
+	    display.appendText(lineBreak);
+	    display.appendText(eventTasks);
+	    display.appendText(lineBreak);
+	    display.appendText(lineBreak);
+	    display.appendText(TASK_FLOATING);
+	    display.appendText(lineBreak);
+	    display.appendText(floatingTasks);
+	    display.appendText(lineBreak);
+	    display.appendText(lineBreak);
+	    display.appendText(TASK_RECURRING);
+	    display.appendText(lineBreak);
+	    display.appendText(recurringTasks);
 	}
 
-	/**
-	 * Method updates the information display from Logic to the user so they can
-	 * preview the information stored in SmarTask.
-	 * 
-	 * @throws ParseException
-	 */
-	private void updateDisplay() throws ParseException {
-		logDisplay = Logic.getMessageLog();
-		deadlineTasks = Logic.getDeadline();
-		eventTasks = Logic.getEvents();
-		floatingTasks = Logic.getFloatingTask();
-		recurringTasks = Logic.getRecurringTask();
-		updateWindows(displayWindow, logDisplay);
-		updateWindows(taskWindow);
-	}
+    /**
+     * Checks the keys pressed by the user and activates certain events
+     * according to which keys have been pressed.
+     * 
+     * @param ke
+     *            the key pressed by the user as recorded by the system
+     */
+    @FXML
+    public void keyboardLog(KeyEvent ke) {
+        if (ke.getCode() == KeyCode.ENTER) {
+            enterKeyEvent();
+        } else if (ke.getCode() == KeyCode.UP) {
+            upKeyEvent();
+        } else if (ke.getCode() == KeyCode.DOWN) {
+            downKeyEvent();
+        } else if (crtlZ.match(ke)) {
+            controlZKeyEvent();
+        } else if (crtlY.match(ke)) {
+            controlYKeyEvent();
+        }
+    }
 
-	private void updateWindows(TextArea display, String toDisplay) {
-		display.clear();
-		display.setText(toDisplay);
-		String emptyString = "";
-		display.appendText(emptyString);
-	}
-
-	private void updateWindows(TextArea display) {
-		display.clear();
-		String lineBreak = "\n";
-		display.setText(TASK_DEADLINE);
-		display.appendText(lineBreak);
-		display.appendText(deadlineTasks);
-		display.appendText(lineBreak);
-		display.appendText(lineBreak);
-		display.appendText(TASK_EVENTS);
-		display.appendText(lineBreak);
-		display.appendText(eventTasks);
-		display.appendText(lineBreak);
-		display.appendText(lineBreak);
-		display.appendText(TASK_FLOATING);
-		display.appendText(lineBreak);
-		display.appendText(floatingTasks);
-		display.appendText(lineBreak);
-		display.appendText(lineBreak);
-		display.appendText(TASK_RECURRING);
-		display.appendText(lineBreak);
-		display.appendText(recurringTasks);
-	}
-
-	/**
-	 * Checks the keys pressed by the user and activates certain events
-	 * according to which keys have been pressed.
-	 * 
-	 * @param ke
-	 *            the key pressed by the user as recorded by the system
-	 */
-	@FXML
-	public void keyboardLog(KeyEvent ke) {
-		if (ke.getCode() == KeyCode.ENTER) {
-			enterKeyEvent();
-		} else if (ke.getCode() == KeyCode.UP) {
-			upKeyEvent();
-		} else if (ke.getCode() == KeyCode.DOWN) {
-			downKeyEvent();
-		} else if (crtlZ.match(ke)) {
-			controlZKeyEvent();
-		} else if (crtlY.match(ke)) {
-			controlYKeyEvent();
-		}
-	}
-
-	/**
-	 * Checks the keys pressed by the user and activates certain events
-	 * according to which keys have been pressed.
-	 * 
-	 * @param ke
-	 *            the key pressed by the user as recorded by the system
-	 */
-	@FXML
-	public void specialKeyboardLog(KeyEvent ke) {
-		if (ke.getCode() == KeyCode.UP) {
-			upKeyEvent();
-		} else if (ke.getCode() == KeyCode.DOWN) {
-			downKeyEvent();
-		} else if (crtlZ.match(ke)) {
-			controlZKeyEvent();
-		} else if (crtlY.match(ke)) {
-			controlYKeyEvent();
-		}
-	}
-
-	private void enterKeyEvent() {
-		String userCommand = inputWindow.getText();
-		
-		if (!poppedCommands.isEmpty()) {
-			String poppedCommand = poppedCommands.pop();
-			pastCommands.push(poppedCommand);
-		}
-		
-		pastCommands.push(userCommand);
-		inputWindow.clear();
-
-		if (userCommand.toLowerCase().equals(COMMAND_EXIT)) {
-			exitCommand();
-		} else if (userCommand.toLowerCase().equals(COMMAND_HELP)) {
-			helpCommand();
-		}
-
-		try {
-			Logic.executeCommand(userCommand);
-			updateDisplay();
-		} catch (Exception e) {
-			System.err.println(MESSAGE_INVALIDCOMMAND);
-		}
-	}
-	
-	private void exitCommand() {
-		Stage stage = (Stage) inputWindow.getScene().getWindow();
-		stage.close();
-	}
-	
-	private void helpCommand() {
-		String helpManual = Help.getHelpManual();
-		updateWindows(displayWindow, helpManual);
-	}
-
-	private void upKeyEvent() {
-		if (pastCommands.isEmpty()) {
-			inputWindow.clear();
-		} else {
-			inputWindow.clear();
-			String pastCommand = pastCommands.pop();
-			poppedCommands.push(pastCommand);
-			inputWindow.setText(pastCommand);
-		}
-	}
-
-	private void downKeyEvent() {
-		if (poppedCommands.isEmpty()) {
-			inputWindow.clear();
-		} else {
-			inputWindow.clear();
-			String pastCommand = poppedCommands.pop();
-			pastCommands.push(pastCommand);
-			inputWindow.setText(pastCommand);
-		}
-	}
-
-	private void controlZKeyEvent() {
-		try {
-			Logic.executeCommand(COMMAND_UNDO);
-			updateDisplay();
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.err.println(MESSAGE_INVALIDUNDO);
-		}
-	}
-
-	private void controlYKeyEvent() {
-		try {
-			Logic.executeCommand(COMMAND_REDO);
-			updateDisplay();
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.err.println(MESSAGE_INVALIDREDO);
-		}
-	}
+    /**
+     * Checks the keys pressed by the user and activates certain events
+     * according to which keys have been pressed.
+     * 
+     * @param ke
+     *            the key pressed by the user as recorded by the system
+     */
+    @FXML
+    public void specialKeyboardLog(KeyEvent ke) {
+        if (ke.getCode() == KeyCode.UP) {
+            upKeyEvent();
+        } else if (ke.getCode() == KeyCode.DOWN) {
+            downKeyEvent();
+        } else if (crtlZ.match(ke)) {
+            controlZKeyEvent();
+        } else if (crtlY.match(ke)) {
+            controlYKeyEvent();
+        }
+    }
+    
+    private void enterKeyEvent() {
+        String userCommand = inputWindow.getText();
+        
+        if (!poppedCommands.isEmpty()) {
+            String poppedCommand = poppedCommands.pop();
+            pastCommands.push(poppedCommand);
+        }
+        
+        pastCommands.push(userCommand);
+        inputWindow.clear();
+        
+        if (userCommand.toLowerCase().equals(COMMAND_EXIT)) {
+            exitCommand();
+        } else if (userCommand.toLowerCase().equals(COMMAND_HELP)) {
+            helpCommand();
+        }
+        
+        try {
+            Logic.executeCommand(userCommand);
+            updateDisplay();
+        } catch (Exception e) {
+            System.err.println(MESSAGE_INVALIDCOMMAND);
+        }
+    }
+    
+    private void exitCommand() {
+        Stage stage = (Stage) inputWindow.getScene().getWindow();
+        stage.close();
+    }
+    
+    private void helpCommand() {
+        String helpManual = Help.getHelpManual();
+        updateWindows(displayWindow, helpManual);
+    }
+    
+    private void upKeyEvent() {
+        if (pastCommands.isEmpty()) {
+            inputWindow.clear();
+        } else {
+            inputWindow.clear();
+            String pastCommand = pastCommands.pop();
+            poppedCommands.push(pastCommand);
+            inputWindow.setText(pastCommand);
+        }
+    }
+    
+    private void downKeyEvent() {
+        if (poppedCommands.isEmpty()) {
+            inputWindow.clear();
+        } else {
+            inputWindow.clear();
+            String pastCommand = poppedCommands.pop();
+            pastCommands.push(pastCommand);
+            inputWindow.setText(pastCommand);
+        }
+    }
+    
+    private void controlZKeyEvent() {
+        try {
+            Logic.executeCommand(COMMAND_UNDO);
+            updateDisplay();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(MESSAGE_INVALIDUNDO);
+        }
+    }
+    
+    private void controlYKeyEvent() {
+        try {
+            Logic.executeCommand(COMMAND_REDO);
+            updateDisplay();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(MESSAGE_INVALIDREDO);
+        }
+   }
 }
