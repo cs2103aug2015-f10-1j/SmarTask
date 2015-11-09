6ef8a1900f20dcc24f83e75a2aec3d82f425bdc9
Zhang Yichi <kevincham1993@gmail.com>
2015-11-09 10:16:01 +0800

    author

--- src/Storage.java
+++ src/Storage.java
@@ -15,7 +15,7 @@ import com.google.gson.JsonSyntaxException;
 /**
  * Storage component - Nothing should be stored in Storage
  * 
- * @author A0130034A
+ * @@author A0130034A
  */
 
 public class Storage {
